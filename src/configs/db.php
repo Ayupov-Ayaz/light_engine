ce5be137707d8ead8ddebc0219aff4392c3f80bc
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-12-02 00:07:12 +0300

    changed db configs

--- src/configs/db.php
+++ src/configs/db.php
@@ -2,9 +2,11 @@
 
 
 return [
-    'host' => getenv('DB_HOST'),
-    'database'=> getenv('DB_NAME'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
+    'host'        => getenv('DB_HOST'),
+    'database'    => getenv('DB_NAME'),
+    'user'        => getenv('DB_USER'),
+    'password'    => getenv('DB_PASSWORD'),
     'date_format' => 'd.m.Y',
+    'charset'     => 'cp1251',
+    'driver'      => 'sqlsrv'
 ];
