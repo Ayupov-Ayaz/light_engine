8d496c61bacda3986f0eba3a6a16c2118cb93765
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-12-02 00:08:41 +0300

    added interface iSingletonDbConnection

---
+++ src/lib/db/iSingletonDbConnection.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ayupov Ayaz
+ * Date: 01.12.18
+ * Time: 14:14
+ */
+
+namespace App\lib\db;
+
+interface iSingletonDbConnection
+{
+    /**
+     * Singleton connection to database
+     * @return mixed
+     */
+    public static function getInstance();
+}
