0092d30423537be773b773adf2922612c696ca63
Аюпов Аяз <ayaz.ayupov@gmail.com>
2018-12-03 21:19:35 +0300

    fix authorization view

--- src/views/login/in.php
+++ src/views/login/in.php
@@ -6,18 +6,18 @@
             <form class="form-horizontal" method="post" action="/login/auth">
                 <span class="heading">АВТОРИЗАЦИЯ</span>
                 <div class="form-group">
-                    <input type="email" class="form-control" id="inputEmail" placeholder="E-mail">
+                    <input type="text" name="login" class="form-control" id="login" placeholder="login">
                     <i class="fa fa-user"></i>
                 </div>
                 <div class="form-group help">
-                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Password">
                     <i class="fa fa-lock"></i>
                     <a href="#" class="fa fa-question-circle"></a>
                 </div>
                 <div class="form-group">
                     <div class="main-checkbox">
-                        <input type="checkbox" value="none" id="checkbox1" name="check"/>
-                        <label for="checkbox1"></label>
+                        <input type="checkbox" value="none" id="remember" name="remember"/>
+                        <label for="remember"></label>
                     </div>
                     <span class="text">Запомнить</span>
                     <button type="submit" class="btn btn-default">ВХОД</button>
